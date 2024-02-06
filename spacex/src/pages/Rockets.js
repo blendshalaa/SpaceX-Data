2389d1a931a0a0f772401552d2798194de591c64
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2024-02-06 15:50:08 +0100

    navlinks

--- spacex/src/pages/Rockets.js
+++ spacex/src/pages/Rockets.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function Rockets() {
   return (
-    <div>Rockets</div>
+    <div>Rockets
+     
+
+
+
+<NavLink to="/missions" className='btn' activeClassName='active-link'>Go to Missions</NavLink>
+<NavLink to="/profiles" className='btn' activeClassName='active-link'>Go to Profiles</NavLink>
+<NavLink to="/dragons" className='btn' activeClassName='active-link'>Go to Dragons</NavLink>
+
+    </div>
   )
 }
 

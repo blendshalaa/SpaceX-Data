fb3536c9e580f03dccc0c2375093b712a6bcea6d
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2024-02-06 15:53:29 +0100

    fetch data for rockets

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -6,39 +6,39 @@ import MyProfile from './pages/MyProfile';
 import NotFound from './pages/NotFound';
 import Rockets from './pages/Rockets';
 import './styles/App.css';
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router-dom';
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/dragons',
-    element:<Dragons/>,
-    errorElement:<NotFound/>
+    path: '/dragons',
+    element: <Dragons />,
+    errorElement: <NotFound />
   }
-  ,{
-    path:'/missions',
-    element:<Missions/>,
-    errorElement:<NotFound/>
-  },{
-    path:'/profiles',
-    element:<MyProfile/>,
-    errorElement:<NotFound/>
+  , {
+    path: '/missions',
+    element: <Missions />,
+    errorElement: <NotFound />
+  }, {
+    path: '/profiles',
+    element: <MyProfile />,
+    errorElement: <NotFound />
   },
   {
-    path:'/',
-    element:<Rockets/>,
-    errorElement:<NotFound/>
+    path: '/',
+    element: <Rockets />,
+    errorElement: <NotFound />
   }
 ])
 
 
 function App() {
   return (
-<>
-<div>
-<RouterProvider router={router}/>
-</div>
-</>
+    <>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </>
   );
 }
 
--- spacex/src/pages/Rockets.js
+++ spacex/src/pages/Rockets.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import rocketServices from '../services/rocketServices'
 
 function Rockets() {
+  const [rocketData, setRocketData] = useState([])
+
+  const fetchRockets = async () => {
+    try {
+      const response = await rocketServices.fetchRockets()
+      setRocketData(response.data)
+      console.log(response.data)
+
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+  useEffect(() => {
+    fetchRockets()
+  }, [])
   return (
-    <div>Rockets</div>
+    <div>Rockets
+      {rocketData.map((item) => (
+        <h1>Rocket name: {item.rocket_name}</h1>
+      ))}
+    </div>
   )
 }
 

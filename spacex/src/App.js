284caa7c8696d2b4f2502282ff8bf5d53e659ceb
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2024-02-06 14:11:53 +0100

    routerBranch

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -1,9 +1,43 @@
+/* eslint-disable no-unused-vars */
 
-import './App.css';
+import Dragons from './pages/Dragons';
+import Missions from './pages/Missions';
+import MyProfile from './pages/MyProfile';
+import NotFound from './pages/NotFound';
+import Rockets from './pages/Rockets';
+import './styles/App.css';
+import {createBrowserRouter} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+
+const router=createBrowserRouter([
+  {
+    path:'/dragons',
+    element:<Dragons/>,
+    errorElement:<NotFound/>
+  }
+  ,{
+    path:'/missions',
+    element:<Missions/>,
+    errorElement:<NotFound/>
+  },{
+    path:'/profiles',
+    element:<MyProfile/>,
+    errorElement:<NotFound/>
+  },
+  {
+    path:'/',
+    element:<Rockets/>,
+    errorElement:<NotFound/>
+  }
+])
+
 
 function App() {
   return (
 <>
+<div>
+<RouterProvider router={router}/>
+</div>
 </>
   );
 }

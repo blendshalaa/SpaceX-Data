827226d8d7b8b1243b073363445064740ecd8adf
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2024-02-08 00:27:37 +0100

    Fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,26 @@
 import { NavLink } from "react-router-dom";
+import '../styles/navlink.css'
 
 const Navbar = () => {
   return (
     <ul className="navbar">
       <li>
-        <NavLink to="/" activeClassName="active">
+        <NavLink to="/" activeClassName="active" className="nav-link">
           Rockets
         </NavLink>
       </li>
       <li>
-        <NavLink to="/dragons" activeClassName="active">
+        <NavLink to="/dragons" activeClassName="active" className="nav-link">
           Dragons
         </NavLink>
       </li>
       <li>
-        <NavLink to="/missions" activeClassName="active">
+        <NavLink to="/missions" activeClassName="active" className="nav-link">
           Missions
         </NavLink>
       </li>
       <li>
-        <NavLink to="/my-profile" activeClassName="active">
+        <NavLink to="/my-profile" activeClassName="active" className="nav-link">
           My Profile
         </NavLink>
       </li>

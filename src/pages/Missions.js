a976e55b585d011da9158e5195cca7cb0ca39a21
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2024-02-08 19:25:01 +0100

    Updated

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -22,33 +22,35 @@ function Missions() {
   };
 
   return (
-    <div className="missions-container">
-      <table className="missions-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Join/Leave</th>
-            <th>Member</th>
-          </tr>
-        </thead>
-        <tbody>
-          {missionsData.map((item) => (
-            <tr key={item.mission_id}>
-              <td>{item.mission_id}</td>
-              <td>{item.mission_name}</td>
-              <td>{item.description}</td>
-              <td>
-                <button onClick={() => handleJoinLeave(item.mission_id, item.reserved)}>
-                  {item.reserved ? 'Leave' : 'Join'}
-                </button>
-              </td>
-              <td>{item.reserved ? 'Active Member' : 'Not a Member'}</td>
+    <div className='table'>
+      <div className="missions-container">
+        <table className="missions-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Join/Leave</th>
+              <th>Member</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {missionsData.map((item) => (
+              <tr key={item.mission_id}>
+                <td>{item.mission_id}</td>
+                <td>{item.mission_name}</td>
+                <td>{item.description}</td>
+                <td>
+                  <button onClick={() => handleJoinLeave(item.mission_id, item.reserved)}>
+                    {item.reserved ? 'Leave' : 'Join'}
+                  </button>
+                </td>
+                <td>{item.reserved ? 'Active Member' : 'Not a Member'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
